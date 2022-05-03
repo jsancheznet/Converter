91838b684eb84738aad845fdbef312292f415f1c
jsanchez.net <jsanchezsilvera@gmail.com>
2022-05-03 14:48:27 -0300

    Can't remember all the changes

--- converter.cpp
+++ converter.cpp
@@ -5,45 +5,137 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-/*
-  Imported data is return in an aiScene structure.
-  
-  // Data things to know
-  By default, assimp uses a left hand coordinate system the same as OpenGL. The output face winding is counter clockwise. Use #aiProcess_FlipWindingOrder to get CW data.
+// Inspiracion: https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/model.h
 
-  // Texture things to know
-  The output UV coordinate system has its origin in the lower-left corner. Use the #aiProcess_FlipUVs flag to get UV coordinates with the upper-left corner als origin.
-
-
-  IMPORTANT: All meshes of an imported scene are stored in an array of aiMesh* inside the aiScene.
-  IMPORTANT: materials are stored in an array inside aiScene
-
-   One mesh uses only a single material everywhere - if parts of the model use a different material, this part is moved to a separate mesh at the same node
- */
-
-
-//
-// TODO: Define mesh struct. After that create a vector of meshes, extract everymesh onto this vector. Do the same for materials.
-//
-
-void ProcessNode(aiNode *Node)
+struct vec3
 {
-    // The Root Node is mainly the file name. Children of root contain the actual meshes.
-    aiString Name = Node->mName;
-    printf("%s\n", Name.C_Str());
+    float x, y, z;
+};
+
+struct vec2
+{
+    float x, y;
+};
+
+struct mesh
+{
+    std::string Name;
+    std::vector<vec3> Vertices;
+    std::vector<vec3> Normals;
+    std::vector<vec2> TextureCoordinates;
+    std::vector<unsigned int> Indices;
+};
+
+struct model
+{
+    std::string Name;
+    std::vector<mesh> Meshes;
+    // Materiales
+    // std::string Directory;
+    // std::vector<texture> Textures;
+};
+
+//
+// Globals
+//
+
+model Model;
+
+//
+// Functions
+//
+
+void ProcessMesh(const aiMesh *CurrentMesh, std::vector<mesh> &Meshes)
+{
+    mesh Result;
+    Result.Name = CurrentMesh->mName.C_Str();
+
+    // Extract Mesh Vertices
+    if(CurrentMesh->HasPositions())
+    {
+        for(unsigned int j = 0; j < CurrentMesh->mNumVertices; j++)
+        {
+            vec3 Vertex =
+            {
+                CurrentMesh->mVertices[j].x,
+                CurrentMesh->mVertices[j].y,
+                CurrentMesh->mVertices[j].z,
+            };
+            Result.Vertices.push_back(Vertex);
+        }
+    }
+
+    // Extract Mesh Normals
+    if(CurrentMesh->HasNormals())
+    {
+        for(unsigned int j = 0; j < CurrentMesh->mNumVertices; j++)
+        {
+            vec3 Normal =
+            {
+                CurrentMesh->mNormals[j].x,
+                CurrentMesh->mNormals[j].y,
+                CurrentMesh->mNormals[j].z,
+            };
+            Result.Normals.push_back(Normal);
+        }
+    }
+
+    // Extract Texture Coordinates
+    if(CurrentMesh->HasTextureCoords(0)) // I'm not sure of the 0 parameter, what is this?
+    {
+        for(unsigned int j = 0; j < CurrentMesh->mNumVertices; j++)
+        {
+            vec2 TextureCoordinate =
+            {
+                CurrentMesh->mTextureCoords[0][j].x,
+                CurrentMesh->mTextureCoords[0][j].y,
+            };
+            Result.TextureCoordinates.push_back(TextureCoordinate);
+        }
+    }
+
+    // Extract Faces/Indices
+    if(CurrentMesh->HasFaces())
+    {
+        for(unsigned int j = 0; j < CurrentMesh->mNumFaces; j++)
+        {
+            aiFace Face = CurrentMesh->mFaces[j];
+            for(unsigned int k = 0; k < Face.mNumIndices; k++)
+            {
+                Result.Indices.push_back(Face.mIndices[k]);
+            }
+        }
+    }
+
+    // aiMaterial* Material = Scene->mMaterials[Mesh->mMaterialIndex];
+
+    Meshes.push_back(Result);
+}
+
+void ProcessNode(const aiScene *Scene, const aiNode *Node)
+{
+    // Set the name for the model or mesh
+    if(Node->mParent == NULL)
+    {
+        Model.Name = Node->mName.C_Str();
+    }
+    else
+    {
+        aiString Name = Node->mName;
+    }
 
     // Extract meshes
     int MeshCount = Node->mNumMeshes;
     for(int i = 0; i < MeshCount; i++)
     {
-        // Scene->mMeshes = *Node->mMeshes[i];
-        printf("MeshIndex: %d", Node->mMeshes[i]);
+        // Node->mMeshes is an array that contains indices to the Scene->mMeshes array
+        ProcessMesh(Scene->mMeshes[Node->mMeshes[i]], Model.Meshes);
     }
 
     int ChildrenCount = Node->mNumChildren;
     for(int i = 0; i < ChildrenCount; i++)
     {
-        ProcessNode(Node->mChildren[i]);
+        ProcessNode(Scene, Node->mChildren[i]);
     }
 }
 
@@ -53,49 +145,55 @@ int main(int Argc, char **Argv)
 
     Assimp::Importer Importer;
     // std::string Filename = "cube/cube.obj";
-    std::string Filename = "teapot/teapot.obj";
+    // std::string Filename = "teapot.obj";
+    std::string Filename = "survival/Survival_BackPack_2.fbx";
 
-    const aiScene* Scene = Importer.ReadFile(Filename, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
+    const aiScene* Scene = Importer.ReadFile(Filename, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices | aiProcess_GenNormals);
     if (Scene == nullptr)
     {
         std::cout << Importer.GetErrorString() << std::endl;
         return false;
     }
 
-    // TODO: Extract UV coordinates
-    // TODO: Extract vertex normals
-    // TODO: Extract indices/faces
-    // TODO: Extract Material | All materials are stored in an array of aiMaterial inside the aiScene.
-    // TODO: Extract Material textures  | To test if it is an embedded texture use aiScene::GetEmbeddedTexture.
+    // Let's process the scene, one node at a time
+    ProcessNode(Scene, Scene->mRootNode);
 
-    int MeshCount = Scene->mNumMeshes;
-    int MaterialCount = Scene->mNumMaterials;
-    int AnimationCount = Scene->mNumAnimations;
-    int TextureCount = Scene->mNumTextures;
-    int LightCount = Scene->mNumLights;
-    int CameraCount = Scene->mNumCameras;
-    printf("MeshCount: %d\n", MeshCount);
-    printf("MaterialCount: %d\n", MaterialCount);
-    printf("AnimationCount: %d\n", AnimationCount);
-    printf("TextureCount: %d\n", TextureCount);
-    printf("LightCount: %d\n", LightCount);
-    printf("CameraCount: %d\n", CameraCount);
+    // TODO: Loop over the materials and store them
+    if(Scene->HasMaterials())
+    {
+        aiMaterial *Material = Scene->mMaterials[1];
 
-    // TODO: Extract the vertices from each mesh, you can create obj files by hand to test things
-    // TODO: We could try creating a global std::vector<mesh> and fill it in!
-    // NOTE: We could extract everything first, vertices, materials, textures and store indicies in the mesh structs.
-    // TODO: check aiMesh structure
-    aiNode *RootNode = Scene->mRootNode;
-    ProcessNode(RootNode);
+        aiString MaterialName;
+        Material->Get(AI_MATKEY_NAME, MaterialName);
+        printf("Material Name: %s\n", MaterialName.C_Str());
 
+        aiString Str;
+        Material->GetTexture(aiTextureType_DIFFUSE, 0, &Str);
+        printf("Texture Name: %s\n", Str.C_Str());
 
-    /*  THIS IS FROM LEARNOPENGL.COM model class
-        // model data
-        vector<Mesh> meshes;
-        void processNode(aiNode *node, const aiScene *scene);
-        Mesh processMesh(aiMesh *mesh, const aiScene *scene);
-        vector<textures> Textures;
-     */
+        // Ambient Color
+        aiColor3D AmbientColor;
+        Material->Get(AI_MATKEY_COLOR_AMBIENT, AmbientColor);
+        printf("Ambient Color: %2.2f\t%2.2f\t%2.2f\n", AmbientColor.r, AmbientColor.g, AmbientColor.b);
+
+        // Diffuse Color
+        aiColor3D DiffuseColor;
+        Material->Get(AI_MATKEY_COLOR_DIFFUSE, DiffuseColor);
+        printf("Diffuse Color: %2.2f\t%2.2f\t%2.2f\n", DiffuseColor.r, DiffuseColor.g, DiffuseColor.b);
+
+        // Specular
+        aiColor3D SpecularColor;
+        Material->Get(AI_MATKEY_COLOR_SPECULAR, SpecularColor);
+        printf("Specular Color: %2.2f\t%2.2f\t%2.2f\n", SpecularColor.r, SpecularColor.g, SpecularColor.b);
+
+        int IlluminationModel;
+        Material->Get(AI_MATKEY_SHADING_MODEL, IlluminationModel);
+        printf("Illumination Model: %d\n", IlluminationModel);
+
+        float SpecularExponent;
+        Material->Get(AI_MATKEY_SHININESS, SpecularExponent);
+        printf("Specular Exponent: %2.2f\n", SpecularExponent);
+    }
 
     return 0;
 }
